d3748dc7e008144ccb7a287cf763f7efa02b6b15
work-anishachopra <work.anishachopra@gmail.com>
2025-09-03 09:42:04 +0530

    drag n drop

--- lib/graphql.ts
+++ lib/graphql.ts
@@ -1,5 +1,8 @@
 import { gql } from "@apollo/client";
 
+// ------------------
+// Queries
+// ------------------
 export const GET_BOARDS = gql`
   query GetBoards {
     boards {
@@ -18,9 +21,116 @@ export const GET_BOARDS = gql`
   }
 `;
 
-const ADD_BOARD = gql`
+// ------------------
+// Board Mutations
+// ------------------
+export const ADD_BOARD = gql`
   mutation AddBoard($title: String!) {
     addBoard(title: $title) {
+      id
+      title
+      lists {
+        id
+        title
+      }
+    }
+  }
+`;
+
+export const UPDATE_BOARD = gql`
+  mutation UpdateBoard($id: ID!, $title: String!) {
+    updateBoard(id: $id, title: $title) {
+      id
+      title
+    }
+  }
+`;
+
+export const DELETE_BOARD = gql`
+  mutation DeleteBoard($id: ID!) {
+    deleteBoard(id: $id)
+  }
+`;
+
+// ------------------
+// List Mutations
+// ------------------
+export const ADD_LIST = gql`
+  mutation AddList($boardId: ID!, $title: String!) {
+    addList(boardId: $boardId, title: $title) {
+      id
+      title
+    }
+  }
+`;
+
+export const UPDATE_LIST = gql`
+  mutation UpdateList($id: ID!, $title: String!) {
+    updateList(id: $id, title: $title) {
+      id
+      title
+    }
+  }
+`;
+
+export const DELETE_LIST = gql`
+  mutation DeleteList($id: ID!) {
+    deleteList(id: $id)
+  }
+`;
+
+// ------------------
+// Task Mutations
+// ------------------
+export const ADD_TASK = gql`
+  mutation AddTask($listId: ID!, $title: String!) {
+    addTask(listId: $listId, title: $title) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+export const UPDATE_TASK = gql`
+  mutation UpdateTask($id: ID!, $title: String!) {
+    updateTask(id: $id, title: $title) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+export const DELETE_TASK = gql`
+  mutation DeleteTask($id: ID!) {
+    deleteTask(id: $id)
+  }
+`;
+
+export const TOGGLE_TASK_COMPLETION = gql`
+  mutation ToggleTaskCompletion($taskId: ID!) {
+    toggleTaskCompletion(taskId: $taskId) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+export const MOVE_TASK = gql`
+  mutation MoveTask(
+    $taskId: ID!
+    $sourceListId: ID!
+    $destListId: ID!
+    $newIndex: Int!
+  ) {
+    moveTask(
+      taskId: $taskId
+      sourceListId: $sourceListId
+      destListId: $destListId
+      newIndex: $newIndex
+    ) {
       id
       title
       lists {
@@ -35,38 +145,3 @@ const ADD_BOARD = gql`
     }
   }
 `;
-
-const ADD_LIST = gql`
-  mutation AddList($boardId: ID!, $title: String!) {
-    addList(boardId: $boardId, title: $title) {
-      id
-      title
-      tasks {
-        id
-        title
-        completed
-      }
-    }
-  }
-`;
-
-const ADD_TASK = gql`
-  mutation AddTask($listId: ID!, $title: String!) {
-    addTask(listId: $listId, title: $title) {
-      id
-      title
-      completed
-    }
-  }
-`;
-
-export const TOGGLE_TASK_COMPLETION = gql`
-  mutation ToggleTaskCompletion($taskId: ID!) {
-    toggleTaskCompletion(taskId: $taskId) {
-      id
-      completed
-    }
-  }
-`;
-
-export { ADD_BOARD, ADD_LIST, ADD_TASK };
